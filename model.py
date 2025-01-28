ea91653b077686739d925a7fe03d9cc8324c484c
Esayas Nigussie <esayasnigusie2@gmail.com>
2025-01-28 20:27:22 +0300

    NN model

---
+++ model.py
@@ -0,0 +1,38 @@
+from torch import nn
+
+
+class NN(nn.Module):
+    def __init__(self, input_dim, hidden_dims=[64, 32, 16]):
+        super(NN, self).__init__()
+        
+        layers = []
+        
+        # Input layer
+        layers.append(nn.Linear(input_dim, hidden_dims[0]))
+        layers.append(nn.BatchNorm1d(hidden_dims[0]))
+        layers.append(nn.ReLU())
+        layers.append(nn.Dropout(0.2))
+        
+        # Hidden layers
+        for i in range(len(hidden_dims)-1):
+            layers.append(nn.Linear(hidden_dims[i], hidden_dims[i+1]))
+            layers.append(nn.BatchNorm1d(hidden_dims[i+1]))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(0.2))
+        
+        # Output layer
+        layers.append(nn.Linear(hidden_dims[-1], 1))
+        
+        self.network = nn.Sequential(*layers)
+        
+        # Initialize weights using He initialization
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            nn.init.kaiming_normal_(module.weight, mode='fan_in', nonlinearity='relu')
+            if module.bias is not None:
+                nn.init.constant_(module.bias, 0)
+    
+    def forward(self, x):
+        return self.network(x)
